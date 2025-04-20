a2f2a6134b2d21f5f501120a181983f8067413e0
satyrmont <bhcp3316@gmail.com>
2025-04-20 11:55:07 -0300

    overflow-x contact section fix 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
+import { styled } from "styled-components";
 import Nav from "./components/Nav.component.jsx";
-
 import Logo from "./pages/0-Logo.page.jsx";
 import Intro from "./pages/1-Intro.page.jsx";
 import Experiencia from "./pages/2-Experiencia.page.jsx";
@@ -9,7 +9,6 @@ import QueEsperar from "./pages/4-QueEsperar.page.jsx";
 import PerguntasFrequentes from "./pages/5-PerguntasFrequentes.page.jsx";
 import Contato from "./pages/6-Contato.page.jsx";
 import MainLayout from "./components/MainLayout.component.jsx";
-
 import { MobileProvider } from "./context/mobileContext.jsx";
 
 function App() {
@@ -25,10 +24,18 @@ function App() {
           <QueEsperar />
           <PerguntasFrequentes />
         </MainLayout>
-        <Contato />
+        <OverflowContainer>
+          <Contato />
+        </OverflowContainer>
       </MobileProvider>
     </React.Fragment>
   );
 }
+
+const OverflowContainer = styled.div`
+  width: 100%;
+  max-width: 100vw;
+  overflow-x: hidden;
+`;
 
 export default App;
